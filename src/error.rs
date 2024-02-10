b85f76373a79a8a54bd87a5fa16ee03081c63aa4
Łukasz Klimek <klinn91@gmail.com>
2024-02-10 20:08:12 +0100

    implemented custom error message (will be reported to user)

--- src/error.rs
+++ src/error.rs
@@ -1,4 +1,5 @@
-#[derive(Debug)]
+use std::fmt::Debug;
+
 pub enum E {
     IO(std::io::Error),
     InputEncodingError,
@@ -26,3 +27,31 @@ impl From<std::str::Utf8Error> for E {
 }
 
 pub type R = Result<(), E>;
+
+impl Debug for E {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            E::IO(e) => {
+                f.write_fmt(format_args!("{e:?}"))?;
+                Ok(())
+            }
+            E::InputEncodingError => {
+                f.write_str("Incorrect input encoding - expected UTF-8")?;
+                Ok(())
+            }
+            E::LexingFailed { line, column } => {
+                f.write_fmt(format_args!("Unknown token (line={line}, column={column})"))?;
+                Ok(())
+            }
+            E::LexingTriviaFailed => {
+                f.write_str("Failed to lex trivia - this is likely a bug")?;
+                Ok(())
+            }
+            E::ParsingFailed => {
+                // TODO: error should include this
+                f.write_str("Unexpected token \"???\" (line=???, column=???)")?;
+                Ok(())
+            }
+        }
+    }
+}
