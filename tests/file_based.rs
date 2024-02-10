26df5edb0f1503ccb4003a88ac80dcf60dad1c39
Łukasz Klimek <klinn91@gmail.com>
2024-02-10 20:09:03 +0100

    test checking lexing error message

--- tests/file_based.rs
+++ tests/file_based.rs
@@ -11,6 +11,7 @@ fn process_vcl(data: &[u8], indent: &str) -> Output {
         .arg("-")
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
         .spawn()
         .expect("Failed to execute command");
 
@@ -23,13 +24,23 @@ fn process_vcl(data: &[u8], indent: &str) -> Output {
     child.wait_with_output().unwrap()
 }
 
+fn verify_formatting(input: &[u8], expected: &[u8], indent: &str) {
+    let output = process_vcl(input, indent);
+    assert_eq!(&output.stdout[..], expected);
+}
+
+fn verify_error(input: &[u8], expected: &[u8], indent: &str) {
+    let output = process_vcl(input, indent);
+    assert_eq!(&output.stderr[..], expected);
+}
+
 fn verify_unchanged(data: &[u8], indent: &str) {
-    let output = process_vcl(data, indent);
-    assert_eq!(&output.stdout[..], data);
+    verify_formatting(data, data, indent)
 }
 
 const EXAMPLE_GOOD: &[u8] = include_bytes!("files/example/good.vcl");
 const NESTED_EXPR_GOOD: &[u8] = include_bytes!("files/nested_expr/good.vcl");
+const CORRUPTED_UNKNOWN_TOKEN: &[u8] = include_bytes!("files/corrupted/unknown_token.vcl");
 
 #[test]
 fn example_stays_unchanged() {
@@ -39,4 +50,13 @@ fn example_stays_unchanged() {
 #[test]
 fn nested_expr_stays_unchanged() {
     verify_unchanged(NESTED_EXPR_GOOD, "4")
+}
+
+#[test]
+fn correct_unknown_token_error() {
+    verify_error(
+        CORRUPTED_UNKNOWN_TOKEN,
+        b"Error: Unknown token (line=3, column=1)\n",
+        "4",
+    );
 }
